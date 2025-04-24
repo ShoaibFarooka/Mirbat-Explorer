e5a7d8419f568dbcdcb034506c8a3f4160ad626d
Abdul Haseeb <chabdulhaseeb222@gmail.com>
2025-04-24 12:25:51 +0500

    Dashboard minor changes in text alignment

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import routes from './routerconfig.jsx';
+import routes from './RouterConfig.jsx'
 import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute.jsx'
 
 const Router = () => {
